279fa7e9dba969bf6b6bf857e300915913a6ef21
Ignl94 <theenthusedwriter@gmail.com>
2021-02-03 14:56:47 -0500

    More Additions
    Started app.py file to link templates and static files together.

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
-//Python
+from flask import Flask, render_template, request
 
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# -------------------------------- END -------------------------------
+if __name__ == "__main__":
+    app.run(debug=True, port=3000)
