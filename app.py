bc35953b26e0344ff569decd16339b6dfc11ae89
Hani Jandali <hanijandali@Hanis-MacBook-Pro.local>
2021-03-07 18:31:09 -0800

    Made the second page (itemsavailable) template, its associated CSS, the products html page and its associated css, and local route for it to be presented

--- app.py
+++ app.py
@@ -2,15 +2,16 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
+# This is the second page displaying items, with template of Inheritance.html and Inheritance.css for products.html and products.css
+
 @app.route('/itemsavailable')
 def info():
-    return render_template('pagetwo.html')
+    return render_template('products.html')
 
 @app.route('/submittal')
 def job():
